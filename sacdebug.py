576c7ff591971d2d55c386170ae4be1f95e9c7d0
Daniel Bell <me@daniel-bell.co.uk>
2014-04-18 18:12:18 +0100

    Fixed a variety of formatting issues and bugs flagged by Pycharm

--- sacdebug.py
+++ sacdebug.py
@@ -1,21 +1,23 @@
 import gdb
+import string
 
 # A list of lists that represents the local variables of
 # the current executed functions
 variable_stack = list()
 # Breakpoint maps of BP Number -> Function/Variable name
-sac_func_bps = dict()   # SACf__ function breakpoints
-sac_return_bps = dict() # SACf__ return breakpoints
-sac_var_bps = dict()    # SaC variable watchpoints
+sac_func_bps = dict()  # SACf__ function breakpoints
+sac_return_bps = dict()  # SACf__ return breakpoints
+sac_var_bps = dict()  # SaC variable watchpoints
 
 # Map of SaC types to their overloaded function identifiers
 # User defined types are mapped to SACt__NAMESPACE__typename
-sac_types = {"int":"i","float":"f", "double":"d","bool":"b"}
+sac_types = {"int": "i", "float": "f", "double": "d", "bool": "b"}
 
 # SaC array descriptor variables (shape, size etc)
 # If the variable referenced exists and is a pointer
 # then these are ignored
 sac_array_exclude = {"__sz", "__dim", "__desc", "__shp"}
+
 
 def sac_vars():
     """Returns a list of valid SaC local variables"""
@@ -28,8 +30,10 @@ def sac_vars():
 
             # TODO Get rid of array descriptor variables
             # Check existence and type of host variable
-
             valid_vars.append(var_name)
+
+    return valid_vars
+
 
 def sac_functions():
     """ Returns a list of user defined and overloaded SaC functions"""
@@ -37,8 +41,9 @@ def sac_functions():
     # Pull the SACf__ function signatures into a list
     func_sig_list = [func_name.strip() for func_name in functions_text.split("\n") if "SACf__" == func_name[:6]]
     # Retrieve only the function name from the signatures into a new list
-    func_list = [func[func.index(" ") + 1 : func.index("(")] for func in func_sig_list]
+    func_list = [func[func.index(" ") + 1: func.index("(")] for func in func_sig_list]
     return func_list
+
 
 def sacvar_to_c(var_name, local_vars):
     """Converts a SaC variable name to the newest C version"""
@@ -50,21 +55,21 @@ def sacvar_to_c(var_name, local_vars):
         # Skip the first 3 underscores to get the proper function signature
         signature = ""
         for ch in var:
-            if underscores < 3
+            if underscores < 3:
                 if ch == "_":
                     underscores += 1
             else:
                 signature += ch
-        valid_vars.append(var)
+        valid_vars.append(signature)
 
     # TODO Check var_name against valid_vars
-
     # If there's more than one variable Single Static Assignment has occured
     # Sort the list to make sure the highest index contains the latest SSA var
     if len(valid_vars) == 1:
         return valid_vars[0]
     else:
         return sorted(valid_vars)[len(valid_vars)]
+
 
 def sacfunc_to_c(func_name, args=list()):
     """Converts a SaC function signature with optional arguments list to it's C version"""
@@ -73,16 +78,17 @@ def sacfunc_to_c(func_name, args=list()):
     # Look for a namespace
     if "::" in func_name:
         # Split the arg by the :: symbol
-        sac_namespace = string.upper(func_name.split("::")[0])
+        sac_namespace = func_name.split("::")[0].upper()
         func_name = func_name.split("::")[1]
-    
+
     c_func_name = "SACf__" + sac_namespace + "__" + func_name
-    
+
     # TODO argument conversion for overloading
     if args:
         pass
 
     return c_func_name
+
 
 def sac_to_c(arg):
     """Converts a SaC variable or function to it's C equivalent"""
@@ -118,40 +124,59 @@ def sac_to_c(arg):
 
     if not is_function:
         print("Variable found")
-        return sacvar_to_c(symbol)
+        return sacvar_to_c(symbol, sac_vars())
     else:
         print("Function found")
         return sacfunc_to_c(symbol, func_args)
 
+
 class SacVariableWatchpoint(gdb.Breakpoint):
-    def __init__(self, spec, func_name):
-        super(SacVariableWatchpoint, self).__init__(spec, gdb.BP_WATCHPOINT,wp_class=gdb.WP_WRITE, internal=True, temporary=True)
+    def __init__(self, spec):
+        super(SacVariableWatchpoint, self).__init__(spec, gdb.BP_WATCHPOINT, wp_class=gdb.WP_WRITE, internal=True,
+                                                    temporary=True)
         self.silent = True
+
     def stop(self):
         return True
+
 
 class SacFunctionBreakpoint(gdb.Breakpoint):
     def __init__(self, spec):
         self.func_name = spec
         super(SacFunctionBreakpoint, self).__init__(spec, gdb.BP_BREAKPOINT, internal=True, temporary=True)
         self.silent = True
+
     def stop(self):
         return True
+
 
 class SacFunctionReturnBreakpoint(gdb.FinishBreakpoint):
     def __init__(self, spec):
         self.func_name = spec
         super(SacFunctionReturnBreakpoint, self).__init__(internal=True)
         self.silent = True
+
     def stop(self):
         return True
 
+
 class SacDebugCommand(gdb.Command):
-    "Initialisation command for SaC debugging facilities"
+    """Command used for performing SaC functions"""
+    
+    def __init(self):
+        super(SacDebugCommand, self).__init__("sac", gdb.COMMAND_SUPPORT)
+
+    def invoke(self, arg, from_tty):
+        pass
+
+
+class SacInitCommand(gdb.Command):
+    """Initialisation command for SaC debugging facilities"""
+
     def __init__(self):
         global sac_func_bps
-        super(SacDebugCommand, self).__init__("sacdebug", gdb.COMMAND_SUPPORT)
-    
+        super(SacInitCommand, self).__init__("sacinit", gdb.COMMAND_SUPPORT)
+
     def invoke(self, arg, from_tty):
         global sac_func_bps
         # Turn off hardware watchpoints because we use way too many
@@ -164,8 +189,9 @@ class SacDebugCommand(gdb.Command):
             sac_func_bps[new_bp.number] = func
             new_bp.silent = True
 
+
 class SacCommand(gdb.Command):
-    "Command for using SaC variables and functions in GDB"
+    """Command for using SaC variables and functions in GDB"""
 
     def __init__(self):
         super().__init__("sac", gdb.COMMAND_SUPPORT)
@@ -173,6 +199,7 @@ class SacCommand(gdb.Command):
     def invoke(self, arg, from_tty):
         if "_sac(" in arg:
             pass
+
 
 def breakpoint_handle(event):
     """Function that is called for any breakpoint stop event"""
@@ -218,15 +245,14 @@ def breakpoint_handle(event):
                 variable_stack.pop()
                 print(variable_stack)
                 valid_points += 1
-            
+
             # Skip over the amount of system defined BPs encountered
             if valid_points == len(event.breakpoints):
-                print("continueing " + str(valid_points))
+                print("continuing " + str(valid_points))
                 gdb.execute("step")
                 # gdb.execute("continue " + str(valid_points - 1))
 
 # Instantiate commands and setup stop event listener
 SacCommand()
-SacDebugCommand()
-SacVariableFunction()
+SacInitCommand()
 gdb.events.stop.connect(breakpoint_handle)
